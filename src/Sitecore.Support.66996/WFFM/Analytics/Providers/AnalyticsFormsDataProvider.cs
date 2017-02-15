9bb7826d8dc13029c19c8096fa5b7eeca48cae8f
Kateryna Protsenko <kpr@sitecore.net>
2017-02-15 15:50:18 +0200

    Fix of the issue 66996

---
+++ src/Sitecore.Support.66996/WFFM/Analytics/Providers/AnalyticsFormsDataProvider.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using Sitecore.Analytics.Reporting;
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.WFFM.Abstractions;
+using Sitecore.WFFM.Abstractions.Analytics;
+using Sitecore.WFFM.Abstractions.Data;
+using Sitecore.WFFM.Abstractions.Shared;
+using Sitecore.WFFM.Analytics.Dependencies;
+using Sitecore.WFFM.Analytics.Queries;
+using IDs = Sitecore.Form.Core.Configuration.IDs;
+using SortDirection = System.Web.UI.WebControls.SortDirection;
+
+namespace Sitecore.Support.WFFM.Analytics.Providers
+{
+    public class AnalyticsFormsDataProvider : Sitecore.WFFM.Analytics.Providers.AnalyticsFormsDataProvider
+    {
+        private readonly ReportDataProviderBase reportDataProvider;
+
+        private readonly Sitecore.WFFM.Abstractions.Shared.ILogger logger;
+
+        private readonly IAnalyticsTracker analyticsTracker;
+
+        private readonly ISettings settings;
+
+        public AnalyticsFormsDataProvider(ReportDataProviderWrapper reportDataProviderWrapper, Sitecore.WFFM.Abstractions.Shared.ILogger logger, IAnalyticsTracker analyticsTracker, ISettings settings) : base(reportDataProviderWrapper, logger, analyticsTracker, settings)
+        {
+            Assert.IsNotNull(reportDataProviderWrapper, "reportDataProviderWrapper");
+            Assert.IsNotNull(logger, "logger");
+            Assert.IsNotNull(analyticsTracker, "analyticsTracker");
+            Assert.ArgumentNotNull(settings, "settings");
+            this.reportDataProvider = reportDataProviderWrapper.GetReportDataProviderBase(false);
+            this.logger = logger;
+            this.analyticsTracker = analyticsTracker;
+            this.settings = settings;
+        }
+
+        public override IEnumerable<IFormContactsResult> GetFormsStatisticsByContact(Guid formId, PageCriteria pageCriteria)
+        {
+            bool flag = !this.settings.IsXdbEnabled;
+            IEnumerable<IFormContactsResult> result;
+            if (flag)
+            {
+                result = new List<IFormContactsResult>();
+            }
+            else
+            {
+                ID formStatisticsByContactsReportQuery = Sitecore.WFFM.Abstractions.Analytics.IDs.FormStatisticsByContactsReportQuery;
+                FormStatisticsByContactReportQuery formStatisticsByContactReportQuery = new FormStatisticsByContactReportQuery(formId, formStatisticsByContactsReportQuery, this.reportDataProvider, null, CachingPolicy.WithCacheDisabled);
+                formStatisticsByContactReportQuery.Execute();
+                result = this.Sort(formStatisticsByContactReportQuery.Data, pageCriteria.Sorting).Skip(pageCriteria.PageIndex).Take(pageCriteria.PageSize);
+            }
+            return result;
+        }
+
+        private Data.SortCriteria CreateSortCriteria(PageCriteria pageCriteria)
+        {
+            if (pageCriteria.Sorting != null)
+            {
+                System.Web.UI.WebControls.SortDirection direction = pageCriteria.Sorting.Direction ==
+                                          Sitecore.WFFM.Abstractions.Data.SortDirection.Asc
+                    ? System.Web.UI.WebControls.SortDirection.Ascending 
+                    : SortDirection.Descending;
+                return new Data.SortCriteria(pageCriteria.Sorting.Field, direction);
+            }
+            return null;
+        }
+
+
+        private IQueryable<IFormContactsResult> Sort(IQueryable<IFormContactsResult> unsortedList, Sitecore.WFFM.Abstractions.Data.SortCriteria sortCriteria)
+        {
+            if (sortCriteria == null)
+            {
+                return unsortedList;
+            }
+            try
+            {
+                IQueryable<IFormContactsResult> result;
+                if (sortCriteria.Direction != Sitecore.WFFM.Abstractions.Data.SortDirection.Desc)
+                {
+                    result = (from info in unsortedList
+                              orderby DataBinder.Eval(info, sortCriteria.Field)
+                              select info);
+                }
+                else
+                {
+                    result = (from info in unsortedList
+                              orderby DataBinder.Eval(info, sortCriteria.Field) descending
+                              select info);
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                this.logger.Log(string.Format("An error occurred during messages sorting with {0} argument.", sortCriteria.Field), this, LogMessageType.Error);
+                return unsortedList;
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.66996/WFFM/Services/Requests/Controllers/SupportFormReportsController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Support.WFFM.Analytics.Providers.Utils;
+
+using Sitecore.WFFM.Abstractions.Analytics;
+using Sitecore.WFFM.Abstractions.Data;
+using Sitecore.WFFM.Abstractions.Shared;
+using Sitecore.WFFM.Services.Filters;
+using Sitecore.Web.Http.Filters;
+using Sitecore.WFFM.Abstractions.Dependencies;
+
+namespace Sitecore.Support.WFFM.Services.Requests.Controllers
+{
+    [AuthorizeSitecore(Roles = "sitecore\\Sitecore Client Users")]
+    public class SupportFormReportsController : Controller
+    {
+
+        private readonly IItemRepository itemRepository;
+        public IWffmDataProvider FormsDataProvider { get; private set; }
+
+        public SupportFormReportsController()
+                : this(DependenciesManager.DataProvider, DependenciesManager.Resolve<IItemRepository>())
+        {
+        }
+
+        public SupportFormReportsController(IWffmDataProvider formsDataProvider, IItemRepository itemRepository)
+        {
+            Assert.ArgumentNotNull(formsDataProvider, "formsDataProvider");
+            Assert.ArgumentNotNull(itemRepository, "itemRepository");
+            this.FormsDataProvider = formsDataProvider;
+            this.itemRepository = itemRepository;
+        }
+
+        [ValidateHttpAntiForgeryToken]
+        public ActionResult GetFormContactsPage(Guid id, PageCriteria pageCriteria, string sortCriteria)
+        {
+            Assert.ArgumentNotNull(pageCriteria, "pageCriteria");
+            pageCriteria.Sorting = SortingUtil.Create(sortCriteria);
+            IEnumerable<IFormContactsResult> formsStatisticsByContact =
+                this.FormsDataProvider.GetFormsStatisticsByContact(id, pageCriteria);
+            return base.Json(new
+            {
+                Items = JsonConvert.SerializeObject(formsStatisticsByContact),
+                HasResults = formsStatisticsByContact.Any<IFormContactsResult>()
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [ValidateHttpAntiForgeryToken]
+        public ActionResult GetFormSummary(Guid id)
+        {
+            IFormStatistics formStatistics = this.FormsDataProvider.GetFormStatistics(id);
+            Item itemFromMasterDatabase = this.itemRepository.GetItemFromMasterDatabase(new ID(id));
+            string displayName = itemFromMasterDatabase.DisplayName;
+            formStatistics.Title = displayName;
+            return base.Json(JsonConvert.SerializeObject(formStatistics), JsonRequestBehavior.AllowGet);
+        }
+
+        [ValidateHttpAntiForgeryToken]
+        public ActionResult GetFormFieldsStatistics(Guid id)
+        {
+            IEnumerable<IFormFieldStatistics> formFieldsStatistics =
+                this.FormsDataProvider.GetFormFieldsStatistics(id);
+            foreach (IFormFieldStatistics current in formFieldsStatistics)
+            {
+                Item itemFromMasterDatabase = this.itemRepository.GetItemFromMasterDatabase(new ID(current.FieldId));
+                if (itemFromMasterDatabase != null)
+                {
+                    IFieldItem fieldItem = this.itemRepository.CreateFieldItem(itemFromMasterDatabase);
+                    if (fieldItem.ParametersDictionary.ContainsKey("islist"))
+                    {
+                        current.IsList = true;
+                    }
+                }
+            }
+            return base.Json(new
+            {
+                Items = JsonConvert.SerializeObject(formFieldsStatistics),
+                legend = DependenciesManager.TranslationProvider.Text("Responses")
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
